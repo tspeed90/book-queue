634a4025e70223fc2ef7698c39864fb0a703005d
tspeed90 <tspeed90@gmail.com>
2018-07-16 23:00:01 +0100

    add dummy data for testing

---
+++ src/components/Browse/testData/testData.js
@@ -0,0 +1,245 @@
+export const paperTowns = {
+  kind: 'books#volumes',
+  totalItems: 39,
+  items: [
+    {
+      kind: 'books#volume',
+      id: 'MLacAgAAQBAJ',
+      etag: 'slHCm5bWAxQ',
+      selfLink: 'https://www.googleapis.com/books/v1/volumes/MLacAgAAQBAJ',
+      volumeInfo: {
+        title: 'Paper Towns',
+        authors: ['John Green'],
+        publisher: 'A&C Black',
+        publishedDate: '2013',
+        description:
+          "Special edition slipcase edition of John Green's Paper Towns, with pop-up paper town. From the bestselling author of The Fault in our Stars. Quentin Jacobsen has always loved Margo Roth Spiegelman, for Margo (and her adventures) are the stuff of legend at their high school. So when she one day climbs through his window and summons him on an all-night road trip of revenge he cannot help but follow. But the next day Margo doesn't come to school and a week later she is still missing. Q soon learns that there are clues in her disappearance . . . and they are for him. But as he gets deeper into the mystery - culminating in another awesome road trip across America - he becomes less sure of who and what he is looking for. Masterfully written by John Green, this is a thoughtful, insightful and hilarious coming-of-age story.",
+        industryIdentifiers: [
+          {
+            type: 'ISBN_13',
+            identifier: '9781408848180'
+          },
+          {
+            type: 'ISBN_10',
+            identifier: '140884818X'
+          }
+        ],
+        readingModes: {
+          text: false,
+          image: true
+        },
+        pageCount: 305,
+        printType: 'BOOK',
+        categories: ['Bildungsromans'],
+        averageRating: 4,
+        ratingsCount: 276,
+        maturityRating: 'NOT_MATURE',
+        allowAnonLogging: false,
+        contentVersion: 'preview-1.0.0',
+        imageLinks: {
+          smallThumbnail:
+            'http://books.google.com/books/content?id=MLacAgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+          thumbnail:
+            'http://books.google.com/books/content?id=MLacAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+        },
+        language: 'en',
+        previewLink:
+          'http://books.google.co.uk/books?id=MLacAgAAQBAJ&printsec=frontcover&dq=intitle:PAPER+TOWNS%2Binauthor:John+Green&hl=&cd=1&source=gbs_api',
+        infoLink:
+          'http://books.google.co.uk/books?id=MLacAgAAQBAJ&dq=intitle:PAPER+TOWNS%2Binauthor:John+Green&hl=&source=gbs_api',
+        canonicalVolumeLink:
+          'https://books.google.com/books/about/Paper_Towns.html?hl=&id=MLacAgAAQBAJ'
+      },
+      saleInfo: {
+        country: 'GB',
+        saleability: 'NOT_FOR_SALE',
+        isEbook: false
+      },
+      accessInfo: {
+        country: 'GB',
+        viewability: 'PARTIAL',
+        embeddable: true,
+        publicDomain: false,
+        textToSpeechPermission: 'ALLOWED',
+        epub: {
+          isAvailable: false
+        },
+        pdf: {
+          isAvailable: false
+        },
+        webReaderLink:
+          'http://play.google.com/books/reader?id=MLacAgAAQBAJ&hl=&printsec=frontcover&source=gbs_api',
+        accessViewStatus: 'SAMPLE',
+        quoteSharingAllowed: false
+      },
+      searchInfo: {
+        textSnippet:
+          'But as he gets deeper into the mystery - culminating in another awesome road trip across America - he becomes less sure of who and what he is looking for.Masterfully written by John Green, this is a thoughtful, insightful and hilarious ...'
+      }
+    }
+  ]
+};
+
+export const missPeregrine = {
+  kind: 'books#volumes',
+  totalItems: 9,
+  items: [
+    {
+      kind: 'books#volume',
+      id: 'acjglgEACAAJ',
+      etag: '9ujGCYdGb24',
+      selfLink: 'https://www.googleapis.com/books/v1/volumes/acjglgEACAAJ',
+      volumeInfo: {
+        title: "Miss Peregrine's Home for Peculiar Children: The Graphic Novel",
+        authors: ['Ransom Riggs'],
+        publisher: 'Yen Press',
+        publishedDate: '2013-10-29',
+        description:
+          "When Jacob Portman was a boy, his grandfather regaled him with stories of his fantastic life at Miss Peregrine's home during the Second World War, even sharing photos of the remarkable children with whom he resided. As Jacob grew up, though, he decided that these photos were obvious fakes, simple forgeries designed to stir up his youthful imagination. Or were they...? Following his grandfather's death - a scene Jacob literally couldn't believe with his own eyes - the sixteen-year-old boy embarks on a mission to disentangle fact from fiction in his grandfather's tall tales. But even his grandfather's elaborate yarns couldn't prepare Jacob for the eccentricities he will discover at Miss Peregrine's Home for Peculiar Children!",
+        industryIdentifiers: [
+          {
+            type: 'ISBN_10',
+            identifier: '0316245283'
+          },
+          {
+            type: 'ISBN_13',
+            identifier: '9780316245289'
+          }
+        ],
+        readingModes: {
+          text: false,
+          image: false
+        },
+        pageCount: 272,
+        printType: 'BOOK',
+        categories: ['Comics & Graphic Novels'],
+        averageRating: 4,
+        ratingsCount: 10,
+        maturityRating: 'NOT_MATURE',
+        allowAnonLogging: false,
+        contentVersion: 'preview-1.0.0',
+        imageLinks: {
+          smallThumbnail:
+            'http://books.google.com/books/content?id=acjglgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+          thumbnail:
+            'http://books.google.com/books/content?id=acjglgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+        },
+        language: 'en',
+        previewLink:
+          'http://books.google.co.uk/books?id=acjglgEACAAJ&dq=intitle:MISS+PEREGRINE%27S+HOME+FOR+PECULIAR+CHILDREN%2Binauthor:Ransom+Riggs+and+Cassandra+Jean&hl=&cd=1&source=gbs_api',
+        infoLink:
+          'http://books.google.co.uk/books?id=acjglgEACAAJ&dq=intitle:MISS+PEREGRINE%27S+HOME+FOR+PECULIAR+CHILDREN%2Binauthor:Ransom+Riggs+and+Cassandra+Jean&hl=&source=gbs_api',
+        canonicalVolumeLink:
+          'https://books.google.com/books/about/Miss_Peregrine_s_Home_for_Peculiar_Child.html?hl=&id=acjglgEACAAJ'
+      },
+      saleInfo: {
+        country: 'GB',
+        saleability: 'NOT_FOR_SALE',
+        isEbook: false
+      },
+      accessInfo: {
+        country: 'GB',
+        viewability: 'NO_PAGES',
+        embeddable: false,
+        publicDomain: false,
+        textToSpeechPermission: 'ALLOWED',
+        epub: {
+          isAvailable: false
+        },
+        pdf: {
+          isAvailable: false
+        },
+        webReaderLink:
+          'http://play.google.com/books/reader?id=acjglgEACAAJ&hl=&printsec=frontcover&source=gbs_api',
+        accessViewStatus: 'NONE',
+        quoteSharingAllowed: false
+      },
+      searchInfo: {
+        textSnippet:
+          'When Jacob Portman was a boy, his grandfather regaled him with stories of his fantastic life at Miss Peregrine&#39;s home during the Second World War, even sharing photos of the remarkable children with whom he resided.'
+      }
+    }
+  ]
+};
+
+export const fifthWave = {
+  kind: 'books#volumes',
+  totalItems: 74,
+  items: [
+    {
+      kind: 'books#volume',
+      id: '2wSQ549m3uoC',
+      etag: 'v6S9uCY79zI',
+      selfLink: 'https://www.googleapis.com/books/v1/volumes/2wSQ549m3uoC',
+      volumeInfo: {
+        title: 'The 5th Wave Chapter Sampler',
+        authors: ['Rick Yancey'],
+        publisher: 'Penguin',
+        publishedDate: '2013-04-08',
+        description:
+          "The Passage meets Ender's Game in an epic new series from award-winning author Rick Yancey. After the 1st wave, only darkness remains. After the 2nd, only the lucky escape. And after the 3rd, only the unlucky survive. After the 4th wave, only one rule applies: trust no one. Now, it's the dawn of the 5th wave, and on a lonely stretch of highway, Cassie runs from Them. The beings who only look human, who roam the countryside killing anyone they see. Who have scattered Earth's last survivors. To stay alone is to stay alive, Cassie believes, until she meets Evan Walker. Beguiling and mysterious, Evan Walker may be Cassie's only hope for rescuing her brother--or even saving herself. But Cassie must choose: between trust and despair, between defiance and surrender, between life and death. To give up or to get up. Begin Cassie's journey with free sample chapters from The Fifth Wave.",
+        industryIdentifiers: [
+          {
+            type: 'ISBN_13',
+            identifier: '9781101635360'
+          },
+          {
+            type: 'ISBN_10',
+            identifier: '1101635363'
+          }
+        ],
+        readingModes: {
+          text: true,
+          image: false
+        },
+        pageCount: 70,
+        printType: 'BOOK',
+        categories: ['Fiction'],
+        averageRating: 4,
+        ratingsCount: 1,
+        maturityRating: 'NOT_MATURE',
+        allowAnonLogging: false,
+        contentVersion: '2.3.4.0.preview.2',
+        imageLinks: {
+          smallThumbnail:
+            'http://books.google.com/books/content?id=2wSQ549m3uoC&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+          thumbnail:
+            'http://books.google.com/books/content?id=2wSQ549m3uoC&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+        },
+        language: 'en',
+        previewLink:
+          'http://books.google.co.uk/books?id=2wSQ549m3uoC&dq=intitle:THE+5TH+WAVE%2Binauthor:Rick+Yancey&hl=&cd=1&source=gbs_api',
+        infoLink:
+          'http://books.google.co.uk/books?id=2wSQ549m3uoC&dq=intitle:THE+5TH+WAVE%2Binauthor:Rick+Yancey&hl=&source=gbs_api',
+        canonicalVolumeLink:
+          'https://books.google.com/books/about/The_5th_Wave_Chapter_Sampler.html?hl=&id=2wSQ549m3uoC'
+      },
+      saleInfo: {
+        country: 'GB',
+        saleability: 'NOT_FOR_SALE',
+        isEbook: false
+      },
+      accessInfo: {
+        country: 'GB',
+        viewability: 'NO_PAGES',
+        embeddable: false,
+        publicDomain: false,
+        textToSpeechPermission: 'ALLOWED',
+        epub: {
+          isAvailable: true
+        },
+        pdf: {
+          isAvailable: true
+        },
+        webReaderLink:
+          'http://play.google.com/books/reader?id=2wSQ549m3uoC&hl=&printsec=frontcover&source=gbs_api',
+        accessViewStatus: 'NONE',
+        quoteSharingAllowed: false
+      },
+      searchInfo: {
+        textSnippet:
+          'The Passage meets Ender&#39;s Game in an epic new series from award-winning author Rick Yancey.'
+      }
+    }
+  ]
+};
