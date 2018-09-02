af3b88a5b51d1af7c7ed2615a6dc9f32278f2614
tspeed90 <tspeed90@gmail.com>
2018-09-02 22:19:18 +0100

    style GenreButton component

--- src/components/GenreButton/GenreButton.js
+++ src/components/GenreButton/GenreButton.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import Styled from 'styled-components';
+
+const Button = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #F3E9E9;
+  color: #333;
+  text-align: center;
+  width: 12rem;
+  height: 8rem;
+  margin: 0 auto;
+  border-radius: 5px;
+`;
+
+const GenreLink = Styled(Link)`
+  padding: 2rem;
+`;
 
 const GenreButton = ({ children, genre }) => {
-  return <Link to={`/browse/${genre}`}>{children}</Link>;
+  return (
+    <Button>
+      <GenreLink to={`/browse/${genre}`}>{children}</GenreLink>
+    </Button>
+  );
 };
 
 export default GenreButton;
